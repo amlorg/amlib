a8af4d8391d8e23ec72d4f8460764a5f1cf3bd08
amuliang <982632988@qq.com>
2016-02-16 02:19:50 +0800

    add readme.txt

--- mytest.js
+++ mytest.js
@@ -1,17 +1,7 @@
 #include 'amlib.js'
-try{
 
 var json = amlib.require('json');
 
 var j = {a:1, b:2}
 var str = json.stringify(j);
 alert(str);
-
-
-
-
-
-
-
-
-}catch(err){alert(err.toString()+"\rError line:"+err.line.toString())}
