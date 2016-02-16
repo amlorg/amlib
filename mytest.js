132eede65c819eafeceb0d67cb2b260fc2afb337
amuliang <982632988@qq.com>
2016-02-16 15:46:45 +0800

    readme.md

--- mytest.js
+++ mytest.js
@@ -1,7 +1,12 @@
 #include 'amlib.js'
 
 var json = amlib.require('json');
+var ttc = amlib.require('textTypeCtrl');
 
 var j = {a:1, b:2}
 var str = json.stringify(j);
 alert(str);
+
+var s = '00123qwe456rty0';
+var digit = ttc('digit');
+alert( digit(s) );
